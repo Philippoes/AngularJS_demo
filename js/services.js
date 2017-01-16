486d819e278c3613f19c710c3b07cf45fe3e475a
Philip <Philip.zdb@gmail.com>
2017-01-16 15:11:16 +0100

    Wraps all components in IIFEs

--- js/services.js
+++ js/services.js
@@ -1,16 +1,18 @@
-angular.module("demoApp").service("userService", function () {
-    var collection = [
-        {firstName: "Philip", lastName: "Zudemberg"},
-        {firstName: "Thomas", lastName: "Ochman"},
-        {firstName: "Amber", lastName: "Wilkie"},
-        {firstName: "Raoul", lastName: "Diffouo"}
-    ];
-    return {
-        users: function () {
-            return collection;
-        },
-        add: function (object) {
-            collection.push(object)
+(function () {
+    angular.module("demoApp").service("userService", function () {
+        var collection = [
+            {firstName: "Philip", lastName: "Zudemberg"},
+            {firstName: "Thomas", lastName: "Ochman"},
+            {firstName: "Amber", lastName: "Wilkie"},
+            {firstName: "Raoul", lastName: "Diffouo"}
+        ];
+        return {
+            users: function () {
+                return collection;
+            },
+            add: function (object) {
+                collection.push(object)
+            }
         }
-    }
-});
+    });
+})();
